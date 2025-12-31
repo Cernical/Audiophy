30082ef9b40797218a4d0b2f603ce1fa77cca3d7
Cernical <esdominus@gmail.com>
2025-12-31 17:52:17 +0100

    Creado el módulo del navbar

---
+++ navbar.php
@@ -0,0 +1,19 @@
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="container">
+        <a class="navbar-brand fw-bold" href="#home"><img src="./media/logo-white.png" alt="Audiophy logo" style="width:30%;"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="nav">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link active" href="#home">Descubre</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Géneros</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Lista</a></li>
+            </ul>
+            <button class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#loginModal">
+                <i class="bi bi-person"></i> Iniciar Sesión
+            </button>
+        </div>
+    </div>
+</nav>
